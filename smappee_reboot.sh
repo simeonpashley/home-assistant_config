a00acbd3f3086bb106e2a55fab6d63d22f7e9751
Simeon Pashley <simeon@pashley.org>
2022-06-04 11:16:39 +0100

    catchup

---
+++ smappee_reboot.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo 'Login to smappee'
+curl -s -X POST -H "Accept: application/json" -H "Content-Type: application/json" --data 'admin' http://192.168.88.113/gateway/apipublic/logon
+echo
+echo 'Reboot energy monitor'
+curl -s -X GET -H "Accept: application/json" -H "Content-Type: application/json" http://192.168.88.113/gateway/apipublic/restartSmappee?action=2
+echo
+echo 'DONE'
+exit 0
